a9a7fa90d13083246e75d6b42d98c6b4ad70863d
iningwei <iningwei@outlook.com>
2019-05-19 23:48:33 +0800

    init 14

---
+++ easy/14.longest-common-prefix.cs
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=14 lang=csharp
+ *
+ * [14] Longest Common Prefix
+ */
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        int prefixLen=0;
+        string preFix="";
+        
+
+        if(prefixLen>=1)
+        {
+            
+        }
+
+        return "";
+    }
+}
+
