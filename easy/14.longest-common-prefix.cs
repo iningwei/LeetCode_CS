973a8e949788f1d4d3800ec2d7cf9d174a97ab07
iningwei <iningwei@outlook.com>
2019-05-22 11:16:44 +0800

    finish 14, add 20

--- easy/14.longest-common-prefix.cs
+++ easy/14.longest-common-prefix.cs
@@ -3,18 +3,40 @@
  *
  * [14] Longest Common Prefix
  */
-public class Solution {
-    public string LongestCommonPrefix(string[] strs) {
-        int prefixLen=0;
-        string preFix="";
-        
-
-        if(prefixLen>=1)
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs == null || strs.Length == 0)
         {
-            
+            return "";
         }
 
-        return "";
+        int minLen = strs[0].Length;
+        for (int i = 1; i < strs.Length; i++)
+        {
+            if (strs[i].Length < minLen)
+            {
+                minLen = strs[i].Length;
+            }
+        }
+
+
+        string target = "";
+        for (int i = 0; i < minLen; i++)
+        {
+            string strTmp = strs[0].Substring(0, i + 1);
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (strs[j].Substring(0, i + 1).Equals(strTmp) == false)
+                {
+                    return target;
+                }
+            }
+            target = strTmp;
+        }
+
+        return target;
     }
 }
 
---
+++ easy/20.valid-parentheses.cs
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=20 lang=csharp
+ *
+ * [20] Valid Parentheses
+ */
+public class Solution {
+    public bool IsValid(string s) {
+        
+    }
+}
+
