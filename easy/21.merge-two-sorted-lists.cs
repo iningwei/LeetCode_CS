206d66aa0c47e66bd467a5d108e577e4052bf7f7
iningwei <iningwei@outlook.com>
2019-05-23 23:07:17 +0800

    add 21

---
+++ easy/21.merge-two-sorted-lists.cs
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=21 lang=csharp
+ *
+ * [21] Merge Two Sorted Lists
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        
+    }
+}
+
