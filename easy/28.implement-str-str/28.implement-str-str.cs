357f7030d758cab4b8687cd430a7fb071545e277
iningwei <iningwei@outlook.com>
2019-05-28 14:14:53 +0800

    add 28

---
+++ easy/28.implement-str-str/28.implement-str-str.cs
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=28 lang=csharp
+ *
+ * [28] Implement strStr()
+ */
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        
+    }
+}
+
