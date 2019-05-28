fa8636e6e5f458643a1cd642ac1f02df37e10fd5
iningwei <iningwei@outlook.com>
2019-05-28 14:24:51 +0800

    add 35

---
+++ easy/35.search-insert-position/35.search-insert-position.cs
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=35 lang=csharp
+ *
+ * [35] Search Insert Position
+ */
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        
+    }
+}
+
