a6ee0ad921defbd1cbfb1335a071cf9c0d57e7dc
iningwei <iningwei@outlook.com>
2019-05-28 11:52:13 +0800

    finish 26

--- easy/26.remove-duplicates-from-sorted-array/26.remove-duplicates-from-sorted-array.cs
+++ easy/26.remove-duplicates-from-sorted-array/26.remove-duplicates-from-sorted-array.cs
@@ -3,9 +3,25 @@
  *
  * [26] Remove Duplicates from Sorted Array
  */
-public class Solution {
-    public int RemoveDuplicates(int[] nums) {
-        
+using System.Collections;
+using System.Collections.Generic;
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        Hashtable map = new Hashtable();
+        int index = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.Contains(nums[i]))
+            {
+                continue;
+            }
+            map[nums[i]] = index;
+            nums[index] = nums[i];
+            index++;
+        }
+        return map.Count;
     }
 }
 
