2371c405beab9687d3c8aaa0d906cb7e3e35e52c
iningwei <iningwei@outlook.com>
2019-10-05 23:44:21 +0800

    35

--- easy/35.search-insert-position/35.search-insert-position.cs
+++ easy/35.search-insert-position/35.search-insert-position.cs
@@ -5,7 +5,19 @@
  */
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
-        
+        int len=nums.Length;
+        if(target<=nums[0]){
+            return 0;
+        }
+        if(target>nums[len-1]){
+            return len;
+        }
+        for (int i = 0; i < len; i++)
+        {
+            if(target==nums[i]){
+                return i;
+            }
+        }
     }
 }
 
